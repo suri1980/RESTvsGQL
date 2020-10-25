4aec61b84a7db867ce760348e086f7485de32cd2
Thota Surendra babu <suri1980@gmail.com>
2020-10-25 12:54:06 +0800

    client & server

---
+++ client/GQL/fetch.js
@@ -0,0 +1,29 @@
+function fetchUsers(){
+
+    fetch('http://localhost:4000', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ query: '{ users { id name avatar posts } }' }),
+        })
+    .then(response => response.json())
+    .then(data => {
+        var users = data.data.users;
+        console.log(data.data.users);
+        const userHtml = users.map(user => {
+            return `
+                <div class="card" style="width: 18rem;">
+                <figure class="figure">
+                    <img src="${user.avatar}" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
+                </figure>
+                <div class="card-body">
+                    <h5 class="card-title">${user.name}</h5>
+                    <p class="card-text">Number of posts: ${user.posts}</p>
+                </div>
+                </div><div>&nbsp;</div>
+            `;
+        });
+        document.querySelector("#app").innerHTML = userHtml;
+    });
+}
+
+fetchUsers();
---
+++ client/REST/fetch.js
@@ -0,0 +1,30 @@
+function fetchUsers(){
+    fetch('http://localhost/wp/wp-json/wp/v2/users').then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        console.log(data);
+        const userHtml = data.map(user => {
+            fetch('http://localhost/wp/wp-json/wp/v2/posts?author='+user.id).then (response => {
+                return response.json();
+            }).then(d => {
+                console.log(d);
+                var content = `
+                    <div class="card" style="width: 18rem;">
+                    <figure class="figure">
+                        <img src="${user.avatar_urls[96]}" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
+                    </figure>
+                    <div class="card-body">
+                        <h5 class="card-title">${user.name}</h5>
+                        <p class="card-text">Number of posts: ${d.length}</p>
+                    </div>
+                    </div><div>&nbsp;</div>
+                `;
+                document.querySelector("#app").insertAdjacentHTML("afterbegin", content);
+            });            
+        });
+    }).catch(function (err) {
+        console.warn('Something went wrong.', err);
+    });
+}
+
+fetchUsers();
